1c1788396d0514ce30b3f5d419a77d6649f9d157
emilynorris <emilynorris2303@gmail.com>
2026-01-13 01:09:01 +0000

    Added styling

--- src/ArticleCard.jsx
+++ src/ArticleCard.jsx
@@ -1,21 +1,22 @@
 function ArticleCard ({article}) {
     const { author, title, topic, created_at, votes, article_img_url, comment_count } = article
 
-    const dateFormatted = new Date (created_at).toLocaleString()
+    const dateFormatted = new Date (created_at).toLocaleDateString()
 
     return (
       <li className='articleCard'>
-        <div className='articleImage'>
-          <img src={article_img_url} alt={`Image for ${title}`} />
-        </div>
+        <div className='articleContent'>
+            <div className='articleImage'>
+                <img src={article_img_url} alt={`Image for ${title}`} />
+            </div>
 
-        <div className='articleText'>
-            <h3>{title}</h3>
-            <p>Written by {author}</p>
-            <p>{dateFormatted}</p>
-            <p>{votes} votes</p>
-            <p>{comment_count} comments</p>
-            <p>Topic: {topic}</p>
+            <div className='articleText'>
+                <h3>{title}</h3>
+                <p>Written by {author}, {dateFormatted}</p>
+                <p>{votes} votes, {comment_count} comments</p>
+                <p></p>
+                <p>Topic: {topic.charAt(0).toUpperCase()+topic.slice(1)}</p>
+            </div>
         </div>
       </li>  
     )
